c13c91dda4fa72531cfe516fd57b0b9905166d15
ctokoli <matumasie@gmail.com>
2023-09-20 18:36:22 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+/* eslint-disable no-unused-vars */
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import SignIn from './components/Signin'
+import Signup from './components/Signup'
 import Dashboard from './components/dashboard'
 
 function App() {
   
-  
   return (
-    <>
-      <Dashboard />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Signup />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
